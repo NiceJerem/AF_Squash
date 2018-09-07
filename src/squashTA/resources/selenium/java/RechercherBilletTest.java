4d232b2563a029c02caf5fe407fa6c38daed50ff
Jerem <Nice.jerem06@gmail.com>
2018-09-07 13:37:17 +0200

    retrait librairie

--- src/squashTA/resources/selenium/java/RechercherBilletTest.java
+++ src/squashTA/resources/selenium/java/RechercherBilletTest.java
@@ -14,7 +14,6 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.hamcrest.core.Is;
 
 
 public class RechercherBilletTest {
