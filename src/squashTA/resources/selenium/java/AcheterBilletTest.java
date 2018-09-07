3c17885e3176ec840084ff07ee4ee96669e13e98
Jerem <Nice.jerem06@gmail.com>
2018-09-07 16:35:34 +0200

    ajout d'une méthode de test

--- src/squashTA/resources/selenium/java/AcheterBilletTest.java
+++ src/squashTA/resources/selenium/java/AcheterBilletTest.java
@@ -38,13 +38,14 @@ public class AcheterBilletTest {
     @Test
     public void testAcheterBillet() {
         System.out.println("Debut Acheter Billet");
-        /*
+        
         PageAccueil accueil= new PageAccueil(driver);
         String dateDepart= "20180920";
         String villeDepart="ORY";
         String villeArrivee="NCE";
 
         accueil.BtnCookieClick();
+        /*
         accueil.ChoisirAllerSimple();
         accueil.SaisirDepart(villeDepart);
         accueil.SaisirArrivee(villeArrivee);
