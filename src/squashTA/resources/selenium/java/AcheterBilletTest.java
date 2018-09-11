d4290ffb490df696330a711052d96d8c70a0ad96
Jerem <Nice.jerem06@gmail.com>
2018-09-11 18:52:50 +0200

    modif

--- src/squashTA/resources/selenium/java/AcheterBilletTest.java
+++ src/squashTA/resources/selenium/java/AcheterBilletTest.java
@@ -18,6 +18,10 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.LocalDateTime;
 import org.hamcrest.core.Is;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 //import AF;
 
 public class AcheterBilletTest {
