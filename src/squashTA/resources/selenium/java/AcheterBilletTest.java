945fbbbfbf853166950b195095182c2f92162a8d
Jerem <Nice.jerem06@gmail.com>
2018-09-11 18:57:10 +0200

    modif

--- src/squashTA/resources/selenium/java/AcheterBilletTest.java
+++ src/squashTA/resources/selenium/java/AcheterBilletTest.java
@@ -22,6 +22,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
+
+import org.openqa.selenium.interactions.Actions;
+
 //import AF;
 
 public class AcheterBilletTest {
